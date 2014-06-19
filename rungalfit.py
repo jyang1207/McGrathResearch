2d8a5323ad0152a2ac14ca77757580dfea25802f
Ian <imtibbet@dhip-115.labs.keyes.colby.edu>
2014-06-19 10:39:49 -0400

    code organization, methods with better definitions

--- rungalfit.py
+++ rungalfit.py
@@ -172,72 +172,22 @@ def run_imexam(imageFilename, centerCoords, xStart, xStop, yStart, yStop):
 	return [line, col, mag, radius, b_a, PA]
 	
 
-def run_galfit(imageFilename, galfit_constraint_filename, includeBulgeComponent, includeGaussianSmoothing):
+def write_galfit_single_parameter(imageFilename, galfit_single_parameter_filename,
+									galfit_single_output_filename, 
+									width, height, X, Y, magnitude, rad, BA, angle):
 	'''
-	opens file (parameter) containing list of images and loops over every image, 
-	running galfit and writing the results to the same directory as the images
+	writes the galfit parameter file for the single component
 	
-	calls methods to invoke iraf methods to write the initial galfit param file
-	for each image
-	
-	parameter imageListFilename -
-		the string of the full path filename of the text file holding the list
-		of images on which galfit will be run
-		
-	parameter includeBulgeComponent - 
-		boolean indicating if a bulge should be fit after first pass by galfit
+	parameter imageFilename - the filoename of the image on which galfit is being run
+	parameter galfit_single_parameter_filename - 
+		the filename of the galfit parameter file being written
+	parameter galfit_single_output_filename - 
+		the filename of the file where the output of the galfit run will be written
+	parameter width, height - the area on the image on which to run galfit
+	parameter X, Y, magnitude, rad, BA, angle - 
+		initial guess at the location and description of the single galaxy component
 	'''
-	returnStr = ""
-	
-	# sigma [# pixels] is used for run gauss when blurring
-	sigma = 15
-	
-	# run the method of dimensions.py
-	[directory_location, galaxy_id, filter, cam_number, image_number, height, width
-		] = run_imhead(imageFilename)
-
-	# the x y location of the top left corner of the area to run minmax and imexam on
-	# in the coordinate system of the original image (which would be 0, 0 for the original)
-	xStart = float(width)/2 - 75.0
-	xStop = float(width)/2 + 75.0
-	yStart = float(height)/2 - 75.0
-	yStop = float(height)/2 + 75.0
-	
-	# calls iraf's minmax method, passing image filename as a parameter
-	# with image possible gaussian smoothed, as well as two points
-	# on the image defining the area on which to run minmax 
-	# returns as a list of two strings the coordinates of the max pixel
-	if includeGaussianSmoothing:
-		centerCoords = run_minmax(run_gauss(imageFilename, sigma), 
-									xStart, xStop, yStart, yStop)
-	else:
-		centerCoords = run_minmax(imageFilename, 
-									xStart, xStop, yStart, yStop)
-	
-	# calls iraf's imexam method, passing filename as a parameter
-	# along with center coordinate and two points defining area
-	# returns initial estimates of the returned paramters
-	[Y, X, magnitude, rad, BA, angle
-		] = run_imexam(imageFilename, centerCoords, xStart, xStop, yStart, yStop)
-	
-	Y = str(float(Y) + yStart)
-	X = str(float(X) + xStart)
-	
-	# define filenames, galaxy_id has full path if any
-	filename = (directory_location + galaxy_id + "_" + 
-				image_number + '_cam' + str(cam_number) + '_' + filter)
-	
-	#galfit_output_filename = filename + "_multi.fits"
-	galfit_single_parameter_filename =	filename + '_single_param.txt'
-	galfit_single_output_filename =		filename + "_single_multi.fits"
-	galfit_single_result_filename =		filename + "_single_result.txt"
-	galfit_bulge_parameter_filename =	filename + '_bulge_param.txt'
-	galfit_bulge_output_filename =		filename + "_bulge_multi.fits"
-	galfit_bulge_result_filename =		filename + "_bulge_result.txt"
-	
-	os.system('touch ' + galfit_single_parameter_filename)								
-
-######### writes gathered galfit parameters to file ###########################
+	os.system('touch ' + galfit_single_parameter_filename)
 	WP = open(galfit_single_parameter_filename,'w')
 	WP.write("# IMAGE and GALFIT CONTROL PARAMETERS\n")
 	WP.write("A) " + imageFilename + 
@@ -308,82 +258,176 @@ def run_galfit(imageFilename, galfit_constraint_filename, includeBulgeComponent,
 	WP.write("\n")
 
 	WP.close()
+	
+	
+def get_galfit_bulge_parameter_str(galfit_single_result_filename, 
+							galfit_bulge_output_filename, galfit_constraint_filename, rad):
+	'''
+	reads the results of the first run and returns it as a long string
+	with the output and constraint modified for bulge run and the
+	new bulge component appended
+	
+	parameter galfit_single_result_filename - the file being read
+	
+	returns - the string of the read file, modified for two component
+	'''
+	singleResultFile = open(galfit_single_result_filename, "r")		
+	resultContents = singleResultFile.readlines()
+	singleResultFile.close()
+	
+	# gather info about first fit while modifying output and coonstraint parameters
+	bulgeParam = ""		
+	positionLine = ""
+	magnitudeLine = ""
+	for resultLine in resultContents:
+	
+		if resultLine.strip()[:2] == "B)":
+			bulgeParam = (bulgeParam + "B) " + galfit_bulge_output_filename +
+						"						#Output data image block\n")
+			
+		elif resultLine.strip()[:2] == "G)":
+			bulgeParam = (bulgeParam + "G) " + galfit_constraint_filename + 
+						"						#File with parameter constraints (ASCII file)\n")
+		
+		elif not positionLine and resultLine.strip()[:2] == "1)":
+			positionLine = resultLine.strip().split(" ")
+			bulgeParam = bulgeParam + resultLine
+			
+		elif not magnitudeLine and resultLine.strip()[:2] == "3)":
+			magnitudeLine = resultLine.strip().split(" ")
+			bulgeParam = bulgeParam + resultLine
+			
+		else:
+			bulgeParam = bulgeParam + resultLine
+			
+	# store results of single component run into variables
+	resultX = positionLine[1]
+	resultY = positionLine[2]
+	resultMag = magnitudeLine[1]
+	
+	
+	# write the third component to the end of galfit_single_result_filename
+	bulgeParam = bulgeParam + ("# Componenet number: 3\n")
+	bulgeParam = bulgeParam + (" 0) sersic					#Component type\n")
+	bulgeParam = bulgeParam + (" 1) " + resultX + " " + resultY + " 1	1			#Position x,y\n")
+	bulgeParam = bulgeParam + (" 3) " + resultMag + "	1			#Integrated Magnitude\n")
+	bulgeParam = bulgeParam + (" 4) " + str(rad) + "			1			#R_e (half-light radius)	[pix]\n")
+	bulgeParam = bulgeParam + (" 5) " + "1.0000		1			#Sersic index n (de Vaucouleurs n=4)\n")
+	bulgeParam = bulgeParam + (" 6) 0.0000		0			#	-----\n")
+	bulgeParam = bulgeParam + (" 7) 0.0000		0			#	-----\n")
+	bulgeParam = bulgeParam + (" 8) 0.0000		0			#	-----\n")
+	bulgeParam = bulgeParam + (" 9) 1" + "			1			#Axis ratio (b/a)\n")
+	bulgeParam = bulgeParam + (" 10) 0" + "		1			#Position angle (PA) [deg: Up=0, left=90]\n")
+	bulgeParam = bulgeParam + (" Z) 0							#Leave in [1] or subtract [0] this comp from data?\n")
+	bulgeParam = bulgeParam + ("\n")
+
+	return bulgeParam
+
+
+def run_galfit(imageFilename, galfit_constraint_filename, includeBulgeComponent, includeGaussianSmoothing):
+	'''
+	opens file (parameter) containing list of images and loops over every image, 
+	running galfit and writing the results to the same directory as the images
+	
+	calls methods to invoke iraf methods to write the initial galfit param file
+	for each image
+	
+	parameter imageFilename -
+		the string of the full path filename of the image on which galfit will be run
+		
+	parameter galfit_constraint_filename -
+		the filename of the constraint file. if none, value is "none"
+	
+	parameter includeBulgeComponent - 
+		boolean indicating if a bulge should be fit after first pass by galfit
+		
+	parameter includeGaussianSmoothing - 
+		boolean indicating if a gaussian smoothing should be applied before running minmax
+		
+	returns - string indicating success or failure
+	'''
+	
+	# sigma [# pixels] is used for run gauss when blurring
+	sigma = 15
+	
+	# run iraf's imhead method to get image information
+	[directory_location, galaxy_id, filter, cam_number, image_number, height, width
+		] = run_imhead(imageFilename)
+
+	# the x y location of the top left corner of the area on which to run minmax and imexam
+	# in the coordinate system of the original image (which would be 0, 0 for the original)
+	xStart = float(width)/2 - 75.0
+	xStop = float(width)/2 + 75.0
+	yStart = float(height)/2 - 75.0
+	yStop = float(height)/2 + 75.0
+	
+	# calls iraf's minmax method, passing image filename as a parameter
+	# with image possible gaussian smoothed, as well as two points
+	# on the image defining the area on which to run minmax 
+	# returns as a list of two strings (the coordinates of the max pixel)
+	if includeGaussianSmoothing:
+		centerCoords = run_minmax(run_gauss(imageFilename, sigma), 
+									xStart, xStop, yStart, yStop)
+	else:
+		centerCoords = run_minmax(imageFilename, 
+									xStart, xStop, yStart, yStop)
+	
+	# calls iraf's imexam method, passing filename as a parameter
+	# along with center coordinates and two points defining area
+	# returns initial estimates of the returned paramters
+	[Y, X, magnitude, rad, BA, angle
+		] = run_imexam(imageFilename, centerCoords, xStart, xStop, yStart, yStop)
+	
+	Y = str(float(Y) + yStart)
+	X = str(float(X) + xStart)
+	
+	# define filenames
+	filename = (directory_location + galaxy_id + "_" + 
+				image_number + '_cam' + str(cam_number) + '_' + filter)
+	galfit_single_parameter_filename =	filename + '_single_param.txt'
+	galfit_single_output_filename =		filename + "_single_multi.fits"
+	galfit_single_result_filename =		filename + "_single_result.txt"
+	galfit_bulge_parameter_filename =	filename + '_bulge_param.txt'
+	galfit_bulge_output_filename =		filename + "_bulge_multi.fits"
+	galfit_bulge_result_filename =		filename + "_bulge_result.txt"
+							
+	# writes the single component parameter file, given filenames and galxy parameters
+	write_galfit_single_parameter(imageFilename, galfit_single_parameter_filename,
+									galfit_single_output_filename, 
+									width, height, X, Y, magnitude, rad, BA, angle)
 
 	# run galfit on paramter file
 	os.system('galfit ' + galfit_single_parameter_filename)
 
+	# detects atomic galfit error
+	# If not failure then removes temp files, otherwise returns
 	if os.path.isfile(galfit_single_output_filename):
 		# remove temp files
 		os.system("rm " + "fit.log")
 		os.system("mv galfit.01 " + galfit_single_result_filename)
-		#os.system("mv " + galfit_output_filename + " " + galfit_single_output_filename)
 	else:
-		return "galfit failed on single component, probably mushroom"
+		return "galfit failed on single component, probably mushroom (atomic galfit error)"
 	
+	# done unless command line specified that a second galfit run
+	# should be done by adding a bulge component to the result of the first run
 	if includeBulgeComponent:
-		# here we would write the third component to the end of galfit_result_filename
-		singleResultFile = open(galfit_single_result_filename, "r")		
-		resultContents = singleResultFile.readlines()
-		singleResultFile.close()
-		
-		# gather info about first fit while modifying output and coonstraint parameters
-		
-		bulgeParam = ""		
-		positionLine = ""
-		magnitudeLine = ""
-		for resultLine in resultContents:
-		
-			if resultLine.strip()[:2] == "B)":
-				bulgeParam = (bulgeParam + "B) " + galfit_bulge_output_filename +
-							"						#Output data image block\n")
-				
-			elif resultLine.strip()[:2] == "G)":
-				bulgeParam = (bulgeParam + "G) " + galfit_constraint_filename + 
-							"						#File with parameter constraints (ASCII file)\n")
-			
-			elif not positionLine and resultLine.strip()[:2] == "1)":
-				positionLine = resultLine.strip().split(" ")
-				bulgeParam = bulgeParam + resultLine
-				
-			elif not magnitudeLine and resultLine.strip()[:2] == "3)":
-				magnitudeLine = resultLine.strip().split(" ")
-				bulgeParam = bulgeParam + resultLine
-				
-			else:
-				bulgeParam = bulgeParam + resultLine
-				
-		
-		resultX = positionLine[1]
-		resultY = positionLine[2]
-		
-		resultMag = magnitudeLine[1]
-		
-		
-		#append bulge component using above info
-		
-		bulgeParam = bulgeParam + ("# Componenet number: 3\n")
-		bulgeParam = bulgeParam + (" 0) sersic					#Component type\n")
-		bulgeParam = bulgeParam + (" 1) " + resultX + " " + resultY + " 1	1			#Position x,y\n")
-		bulgeParam = bulgeParam + (" 3) " + resultMag + "	1			#Integrated Magnitude\n")
-		bulgeParam = bulgeParam + (" 4) " + str(rad) + "			1			#R_e (half-light radius)	[pix]\n")
-		bulgeParam = bulgeParam + (" 5) " + "1.0000		1			#Sersic index n (de Vaucouleurs n=4)\n")
-		bulgeParam = bulgeParam + (" 6) 0.0000		0			#	-----\n")
-		bulgeParam = bulgeParam + (" 7) 0.0000		0			#	-----\n")
-		bulgeParam = bulgeParam + (" 8) 0.0000		0			#	-----\n")
-		bulgeParam = bulgeParam + (" 9) 1" + "			1			#Axis ratio (b/a)\n")
-		bulgeParam = bulgeParam + (" 10) 0" + "		1			#Position angle (PA) [deg: Up=0, left=90]\n")
-		bulgeParam = bulgeParam + (" Z) 0							#Leave in [1] or subtract [0] this comp from data?\n")
-		bulgeParam = bulgeParam + ("\n")
+	
+		# reads the results of the first run and returns it as a long string
+		# with the output and constraint modified for bulge run and the
+		# new bulge component appended with some intitial guess parameters
+		bulgeParamStr = get_galfit_bulge_parameter_str(galfit_single_result_filename, 
+							galfit_bulge_output_filename, galfit_constraint_filename, rad)
 
-
+		# write the bulge parameter file using the modified contents of the single results
 		bulgeParamFile = open(galfit_bulge_parameter_filename, "w")
-		bulgeParamFile.write(bulgeParam)
+		bulgeParamFile.write(bulgeParamStr)
 		bulgeParamFile.close()
 		
 		# run galfit on paramter file
 		os.system('galfit ' + galfit_bulge_parameter_filename)
 	
-	
+		# detects atomic galfit error
+		# If not failure then removes temp files, otherwise returns
 		if os.path.isfile(galfit_bulge_output_filename):
 			# remove temp files
 			os.system("rm " + "fit.log")
@@ -391,13 +435,88 @@ def run_galfit(imageFilename, galfit_constraint_filename, includeBulgeComponent,
 			#os.system('mv ' + galfit_output_filename + ' ' + galfit_bulge_output_filename)
 		else:
 			return "galfit failed on bulge component, probably mushroom"
-
+	
+	# if we get here then nothing went wrong!
 	return "success"
 	
 	
+def main(imageListFilename, galfit_constraint_filename, includeBulgeComponent, includeGaussianSmoothing):
+	'''
+	main method loops through all image filenames in image list, running galfit
+	and logging errors to a log file, which is named according to the date and 
+	the images on which galfit was run
+	
+	parameter imageListFilename -
+		the string of the full path filename of the list of images on which galfit will be run
+		
+	parameter galfit_constraint_filename -
+		the filename of the constraint file. if none, value is "none"
+	
+	parameter includeBulgeComponent - 
+		boolean indicating if a bulge should be fit after first pass by galfit
+		
+	parameter includeGaussianSmoothing - 
+		boolean indicating if a gaussian smoothing should be applied before running minmax
+	'''
+	# read list of image filenames from input file
+	inputFile = open(imageListFilename, 'r')
+	imageFilenames = inputFile.readlines()
+	inputFile.close()
+	
+	# this loops through every image in images file and removes new line
+	imageFilenames = [ imageFilename.strip() for imageFilename in imageFilenames ]
+	
+	# set the log header
+	logMsg = "run on " + time.strftime("%m-%d-%Y") + "\n"
+	
+	# this loops through every image in images file and writes the log, running galfit
+	for imageFilename in imageFilenames:
+		logMsg = logMsg + imageFilename + ": "
+		
+		# run galfit, preventing crashes but logging errors in log and printing them
+		try:
+			# galfit returns a string indicating success or some failure
+			logMsg = logMsg + run_galfit(imageFilename, 
+										galfit_constraint_filename, 
+										includeBulgeComponent, includeGaussianSmoothing)
+		
+		# allow user to stop the program running altogether with ctrl-c
+		except KeyboardInterrupt:
+			print ("Escape character ctrl-c used to terminate rungalfit. Log file will still be written.")
+			break
+		
+		# log all runtime errors other than those resulting from code modification typos
+		# move on to the next image regardless
+		except not NameError:
+			errorMsg = str(sys.exc_info()[0]) + str(sys.exc_info()[1])
+			print errorMsg
+			logMsg = logMsg + errorMsg
+		
+		# every image on its own line in the log file
+		logMsg = logMsg + "\n"
+	
+	# create the log file in the same directory as python was called
+	logFilename = ("rungalfit_log_" + 
+					imageFilenames[0].split("/")[-1].split("_")[1].split(".")[1] + "_to_" + 
+					imageFilenames[-1].split("/")[-1].split("_")[1].split(".")[1] +
+					"_" + time.strftime("%m-%d-%Y") + ".txt")
+	
+	# write the log file
+	print ("writing log file to " + logFilename)
+	log = open(logFilename, 'w')
+	log.write(logMsg)
+	log.close()
+	
+	print ("Done!\nIn order to summarize results run sumgalfit.py")
+	
+	
 if __name__ == "__main__":
-
-	usage = "usage: %prog [-h for help] [options] input" 
+	'''
+	parses the command line using the optparse package
+	NOTE: argparse is the current version as of python 2.7, 
+			but optparse is used to maintain better backwards compatibility
+	'''
+	usage = "usage: %prog [-h help] [options] inputFile" 
 
 	# used to parse command line arguments
 	parser = OptionParser(usage)
@@ -429,15 +548,6 @@ if __name__ == "__main__":
 					"must provide an input file containing the list of full path image filenames.")
 	elif not os.path.isfile(args[0]):
 		parser.error("input file " + args[0] + " either does not exist or is not accessible")
-	
-	
-	# read list of image filenames from input file
-	inputFile = open(args[0], 'r')
-	imageFilenames = inputFile.readlines()
-	inputFile.close()
-	
-	# this loops through every image in images file and removes new line
-	imageFilenames = [ imageFilename.strip() for imageFilename in imageFilenames ]
 		
 	# set constraint to default unless one is given on command line
 	if not options.constraint:
@@ -447,43 +557,5 @@ if __name__ == "__main__":
 	else:
 		constraintFilename = options.constraint
 	
-	# set the log header
-	logMsg = "run on " + time.strftime("%m-%d-%Y") + "\n"
-	
-	# this loops through every image in images file and writes the log, running galfit
-	for imageFilename in imageFilenames:
-		logMsg = logMsg + imageFilename + ": "
-		
-		# run galfit, preventing crashes but logging errors in log
-		try:
-			# galfit returns a string indicating success or some failure
-			logMsg = logMsg + run_galfit(imageFilename, constraintFilename, options.bulge, options.gauss)
-			
-		# allow user to stop the program running altogether with ctrl-c
-		except KeyboardInterrupt:
-			print ("Escape character ctrl-c used to terminate rungalfit. Log file will still be written.")
-			break
-		
-		# log all runtime errors, but move on to the next image regardless
-		except:
-			errorMsg = str(sys.exc_info()[0]) + str(sys.exc_info()[1])
-			print errorMsg
-			logMsg = logMsg + errorMsg
-		
-		# every image on its own line in the log file
-		logMsg = logMsg + "\n"
-	
-	# create the log file in the same directory as python was called
-	logFilename = ("rungalfit_log_" + 
-					imageFilenames[0].split("/")[-1].split("_")[1].split(".")[1] + "_to_" + 
-					imageFilenames[-1].split("/")[-1].split("_")[1].split(".")[1] +
-					"_" + time.strftime("%m-%d-%Y") + ".txt")
-	
-	# write the log file
-	print ("writing log file to " + logFilename)
-	log = open(logFilename, 'w')
-	log.write(logMsg)
-	log.close()
-	
-	print ("Done!\nIn order to summarize results run sumgalfit.py")
-######################### done ################################################
+	# pass pertinent info to the main method
+	main(args[0], constraintFilename, options.bulge, options.gauss)
